d232389ffc571eff4159150c43b08d42399327db
abolfazl <bateternali@gmail.com>
2020-01-30 15:12:54 +0330

    first commit

---
+++ commands/ffmpeg/changeVideoSize.sh
@@ -0,0 +1,1 @@
+ffmpeg -i [input].mkv -b [TargetVolume] [output].mkv
---
+++ commands/nginx/staticFilePermission.sh
@@ -0,0 +1,3 @@
+chmod o+x [path]
+chown -R [nginx_name]:[nginx_name] [path]
+# run `ps -aux | grep nginx` and copy nginx process name that in most cases it is www-data
